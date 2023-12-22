a3c229364a1c2cda7fcef12791368e34d6d82978
BURAK ALKAN <73407527+nephtys1001@users.noreply.github.com>
2023-12-22 19:00:59 +0300

    I added project section but I will add my projects later

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./Components/About";
 import Line from "./Components/Line";
 import Navbar from "./Components/Navbar";
+import Projects from "./Components/Projects";
 import Who from "./Components/Who";
 
 
@@ -9,11 +10,18 @@ function App() {
   return (
     <div id="home">
      <Navbar/>
+
      <About/>
 
     <Line />
 
      <Who/>
+
+     <Line />
+
+     <Projects/>
+
+     <Line/>
     
     </div>
   );
---
+++ src/Components/Projects.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import "../Style/Projects.css"
+
+//https://peppy-cuchufli-b7f5bf.netlify.app/
+
+function Projects() {
+
+
+  return (
+    <div className='projects' id='projects'>
+
+        <div className='project-title'>
+        <h1>My Projects</h1>
+        <hr></hr>
+        </div>
+
+        <div className='project'>
+
+            <h2>Soon...</h2>
+
+
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Projects
